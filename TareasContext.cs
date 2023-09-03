180c8ee416099f107249810805c560ebc7842ccf
Carlos Segura <carlos.segurap09@gmail.com>
2023-09-02 20:15:37 -0500

    configuracion de entity framework

---
+++ TareasContext.cs
@@ -0,0 +1,8 @@
+namespace proyectoef;
+
+public class TareasContext: DbContext
+{
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Tarea> Tareas { get; set; }
+    public TareasContext(DbContextOptions<TareasContext> options): base(options){}
+}
