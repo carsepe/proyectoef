7d7c79bf163541cfe758f771f5afe1304efce1a6
Carlos Segura <carlos.segurap09@gmail.com>
2023-09-02 21:05:29 -0500

    Utilizando base de datos en memoria

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -8,6 +8,7 @@ public class Categoria
     public Guid CategoriaId { get; set; }
     
     [Required]
+    [MaxLength(150)]
     public string Nombre { get; set; }
     public string Descripcion{ get; set; }
     public virtual ICollection<Tarea> Tareas {get; set; }
--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,15 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace proyectoef.Models;
 
 public class Tarea
 {
+    [Key]
     public Guid TareaId { get; set; }
+
+    [ForeignKey("CategoriaId")]
     public Guid CategoriaId { get; set; }
+
+    [Required]
+    [MaxLength(200)]
     public string Titulo { get; set; }
     public string Descripcion { get; set; }
     public Prioridad PrioridadTarea { get; set; }
     public DateTime FechaCreacion { get; set; }
     public virtual Categoria Categoria { get; set; }
     public virtual ICollection<Tarea> Tareas { get; set; }
+
+    [NotMapped]
+    public string Resumen { get; set; }
     
 }
 
