d94c1553ea8efff6a5d773279c5c35cb2fd3e83d
Carlos Segura <carlos.segurap09@gmail.com>
2023-09-03 13:04:41 -0500

    Eliminando datos con Entity framework

--- Program.cs
+++ Program.cs
@@ -56,4 +56,14 @@ app.MapPut("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [F
     return Results.NotFound();
 });
 
+app.MapDelete("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromRoute] Guid id) =>
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+
+     if (tareaActual != null)
+     {
+        dbContext.Remove(tareaActual);
+     }
+});
+
 app.Run();
