e712daedccd373d91ee42bf984523f38f7d2e0bf
Carlos Segura <carlos.segurap09@gmail.com>
2023-09-03 12:34:17 -0500

    Guardando datos con Entity framework

--- Program.cs
+++ Program.cs
@@ -21,7 +21,19 @@ app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext) =>
 {
     return Results.Ok(dbContext.Tareas
     .Include(p=> p.Categoria)
-    .Where(p=> p.PrioridadTarea == Prioridad.Baja));
+    );
+});
+
+app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea) =>
+{
+    tarea.TareaId = Guid.NewGuid();
+    tarea.FechaCreacion = DateTime.Now;
+    await dbContext.AddAsync(tarea);
+    //await dbContext.Tareas.AddAsync(tarea);
+
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok();
 });
 
 app.Run();
